e7e480ad0e8d7bf4cf5e1895aceb0e527aaafcfa
Sean Schofield <sean@railsdog.com>
2010-11-05 11:07:01 -0400

    More clean up for rspec version.

--- core/spec/test_template.rb
+++ core/spec/test_template.rb
@@ -1,6 +1,6 @@
 # rails site template for creating a test site to be used in testing
 gem "spree_core", :path => ".."
 gem "devise", ">= 1.1.3"
-gem 'rspec', '>= 2.0.0.beta.19', :group => 'test'
-gem 'rspec-rails', '>= 2.0.0.beta.19', :group => 'test'
+gem 'rspec', '2.0.0.beta.19', :group => 'test'
+gem 'rspec-rails', '2.0.0.beta.19', :group => 'test'
 gem 'fabrication', :group => 'test'
