0f23445edc7344581c72cc516a4dacac46bff0d6
Sean Schofield <sean@railsdog.com>
2010-11-06 15:22:53 -0400

    Got specs working for UserSessionsController by stubbing out Devise methods.

--- auth/spec/controllers/user_sessions_controller_spec.rb
+++ auth/spec/controllers/user_sessions_controller_spec.rb
@@ -1,33 +1,26 @@
 require 'spec_helper'
 
 describe UserSessionsController do
-  def mock_user(stubs={})
-    @mock_user ||= mock_model(User, stubs).as_null_object
-  end
-  
-  before(:each) do
-    request.env['warden'] = mock(Warden, :authenticate => mock_user, :authenticate! => mock_user)
-  end
 
   context "#create" do
     context "when current_order is associated with a guest user" do
-      #let(:user) { mock User }
-      let(:order) { mock_model Order, :user => mock_user }
+      let(:user) { mock User }
+      let(:order) { mock_model Order }
 
       before do
-        controller.stub :authorize!
+        controller.stub :is_devise_resource? => true, :resource_name => nil, :require_no_authentication => nil, :user_signed_in? => true
+        controller.stub_chain :warden, :authenticate!
         controller.stub :current_order => order
       end
 
       it "should associate the order with the newly authenticated user" do
-        registered_user = mock_model User
-        controller.stub :current_user => registered_user
-        order.should_receive(:associate_user!).with registered_user
+        controller.stub :current_user => user
+        order.should_receive(:associate_user!).with user
         post :create, {}, { :order_id => 1 }
       end
 
       it "should destroy the session token for guest_user" do
-        controller.stub :current_user => @mock_user
+        controller.stub :current_user => user
         order.stub :associate_user!
         post :create, {}, { :order_id => 1, :guest_token => "foo" }
         session[:guest_token].should be_nil
